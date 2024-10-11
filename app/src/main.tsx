959aa6044ae5060f6769be8906bfc9fd4993805f
Carlos Castillo <castillo.st@gmail.com>
2024-10-11 20:27:47 +0200

    Fix linter error

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
