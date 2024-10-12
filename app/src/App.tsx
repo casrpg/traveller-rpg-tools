57623525bbc9917d84bad5eeb90ad767a5771964
Carlos Castillo <castillo.st@gmail.com>
2024-10-12 22:48:27 +0200

    WIP reshaping the app with lcars

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,49 +1,69 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-import { Rocket, Users } from 'lucide-react'
 import { NPCGenerator } from './components/NPCGenerator'
 import { PlanetDecoder } from './components/PlanetDecoder'
 
 const App: React.FC = () => {
+  const nav = (
+    <nav id="secondary-nav">
+      <Link to="/">Tool Index</Link>
+      <Link to="/planet-decoder">Planet Decoder</Link>
+      <Link to="/npc-generator">NPC generator</Link>
+    </nav>
+  )
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const routes = (
+    <Routes>
+      <Route path="/npc-generator" element={<NPCGenerator />} />
+      <Route path="/planet-decoder" element={<PlanetDecoder />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+
   return (
     <Router>
-      <div className="min-h-screen bg-black text-[#ff9900]">
-        <header className="bg-[#000033] p-4">
-          <nav className="container mx-auto flex justify-between items-center">
-            <h1 className="text-2xl font-bold text-[#ff9900]">Traveller RPG Tools</h1>
-            <ul className="flex space-x-4">
-              <li>
-                <Link to="/npc-generator" className="lcars-button">
-                  <Users className="inline-block mr-2" />
-                  NPC Generator
-                </Link>
-              </li>
-              <li>
-                <Link to="/planet-decoder" className="lcars-button">
-                  <Rocket className="inline-block mr-2" />
-                  Planet Decoder
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <main className="container mx-auto p-4">
-          <Routes>
-            <Route path="/npc-generator" element={<NPCGenerator />} />
-            <Route path="/planet-decoder" element={<PlanetDecoder />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
+      <div className="wrap-everything">
+          <div className="wrap">
+            <div className="left-frame" data-id-test="menu-container">
+              <div>
+                <div className="panel-3">Traveller RPG <span className="hop">Tools</span></div>
+                {nav}
+              </div>
+              <div className="panel-4">04<span className="hop">-41969</span></div>
+              <div className="panel-5">05<span className="hop">-1701D</span></div>
+              <div className="panel-6">06<span className="hop">-071984</span></div>
+              <div className="panel-7">07<span className="hop">-47148</span></div>
+              <div>
+                <div className="panel-8">08<span className="hop">-091966</span></div>
+              </div>
+            </div>
+            <div className="right-frame">
+              <div className="bar-runner">
+                <div className="bar-6"></div>
+                <div className="bar-7"></div>
+                <div className="bar-8"></div>
+                <div className="bar-9"></div>
+                <div className="bar-10"></div>
+              </div>
+              <main>
+                {routes}
+              </main>
+            </div>
+          </div>
       </div>
     </Router>
   )
 }
 
 const Home: React.FC = () => (
-  <div className="text-center lcars-panel">
-    <h2 className="text-4xl font-bold mb-4 lcars-header">Welcome to Traveller RPG Tools</h2>
-    <p className="mb-4 lcars-content">Select a tool from the navigation menu to get started.</p>
-  </div>
+  <>
+    <img src="/sfcmd.png" className="sfc" />
+    <div className="lcars-heading">Traveller RPG Tools</div>
+    <div className="lcars-access">
+      user authorization code <span className="blink medium-dark-blue">accepted</span>
+    </div>
+  </>
 )
 
 export default App
