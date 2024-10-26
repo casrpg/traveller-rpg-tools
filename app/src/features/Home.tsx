ad78cad9ededc6bb6e2b0e93d39bbe4c9a8347c5
Carlos Castillo <castillo.st@gmail.com>
2024-10-26 18:36:51 +0200

    Update logo and main image

--- app/src/features/Home.tsx
+++ app/src/features/Home.tsx
@@ -1,9 +1,10 @@
 import type { FC } from 'react'
 import { Panel } from '../lcars/Panel'
+import './Home.css'
 
 export const Home: FC = () => (
     <Panel kind="1">
-      <img src="/sfcmd.png" className="sfc" />
+      <img src="/traveller-rpg.webp" className="home--logo" />
       <div className="lcars-heading">Traveller RPG Tools</div>
       <div className="lcars-access">
         user authorization code <span className="blink medium-dark-blue">accepted</span>
