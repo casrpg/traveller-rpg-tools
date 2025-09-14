1b6059ce21ca23ba32c0ba2702c6e171a5f7e2b3
Carlos Castillo <castillo.st@gmail.com>
2025-09-14 18:54:36 +0200

    chore: Avoid react prop validations as we already use typescript for that

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,33 +1,35 @@
-import globals from "globals"
-import pluginJs from "@eslint/js"
-import tseslint from "typescript-eslint"
-import pluginReact from "eslint-plugin-react"
-import love from 'eslint-config-love'
-
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import love from "eslint-config-love";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     ...love,
-    files: ['**/*.js', '**/*.ts'],
+    files: ["**/*.js", "**/*.ts"],
   },
   {
     settings: {
       react: {
-        version: 'detect'
-      }
+        version: "detect",
+      },
     },
     rules: {
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/no-misused-promises': 'off',
       '@typescript-eslint/promise-function-async': 'off',
       'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off', // Disabled because we use TypeScript for prop validation
       'eslint-comments/require-description': 'off',
       'no-console': 'off',
-    }
-  }
+      'quotes': ['error', 'single'],
+      '@typescript-eslint/quotes': ['error', 'single']
+    },
+  },
 ];
