df5f83176140c9085f941a58f5472841dce890ad
Carlos Castillo <castillo.st@gmail.com>
2026-01-11 23:08:27 +0100

    test: format code and update expected values in planet decoder tests

--- tests/test-decode-planet.spec.ts
+++ tests/test-decode-planet.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-const nameLocator = '[data-test-id="name"]'
-const astroportLocator = '[data-test-id="astroport"]'
-const sizeLocator = '[data-test-id="size"]'
-const atmosphereLocator = '[data-test-id="atmosphere"]'
-const hydrographicsLocator = '[data-test-id="hydrographics"]'
-const populationLocator = '[data-test-id="population"]'
-const governmentLocator = '[data-test-id="government"]'
-const lawLevelLocator = '[data-test-id="lawLevel"]'
-const techLevelLocator = '[data-test-id="techLevel"]'
+const nameLocator = '[data-test-id="name"]';
+const astroportLocator = '[data-test-id="astroport"]';
+const sizeLocator = '[data-test-id="size"]';
+const atmosphereLocator = '[data-test-id="atmosphere"]';
+const hydrographicsLocator = '[data-test-id="hydrographics"]';
+const populationLocator = '[data-test-id="population"]';
+const governmentLocator = '[data-test-id="government"]';
+const lawLevelLocator = '[data-test-id="lawLevel"]';
+const techLevelLocator = '[data-test-id="techLevel"]';
 
 test('test decode planet', async ({ page }) => {
   await page.goto('http://localhost:8080/');
@@ -17,13 +17,20 @@ test('test decode planet', async ({ page }) => {
   await page.getByPlaceholder('Enter planet code').fill('AA7294718');
   await page.getByRole('button', { name: 'Decode Planet' }).click();
   await expect(page.locator(nameLocator)).toHaveText('Planet AA7294718');
-  await expect(page.locator(astroportLocator)).toHaveText('quality: Excelent, docking price: 1D x 1.000 Cr, fuel: Refined, services: Repairs and all shipyard');
+  await expect(page.locator(astroportLocator)).toHaveText(
+    'quality: Excelent, docking price: 1D x 1.000 Cr, fuel: Refined, services: Repairs and all shipyard'
+  );
   await expect(page.locator(sizeLocator)).toContainText('diameter: 16.000 Km,');
-  await expect(page.locator(atmosphereLocator)).toContainText('Standard / Tainted');
+  await expect(page.locator(atmosphereLocator)).toContainText(
+    'Standard / Tainted'
+  );
   await expect(page.locator(hydrographicsLocator)).toContainText('16%-25%');
   await expect(page.locator(populationLocator)).toContainText('P.000.000.000');
-  await expect(page.locator(governmentLocator)).toContainText('Balkanisation');
-  await expect(page.locator(lawLevelLocator)).toContainText('poison gas');
-  await expect(page.locator(techLevelLocator)).toContainText('Early Space Age');
+  await expect(page.locator(governmentLocator)).toContainText(
+    'Representative Democracy'
+  );
+  await expect(page.locator(lawLevelLocator)).toContainText('Shotguns banned');
+  await expect(page.locator(techLevelLocator)).toContainText(
+    'Bronze/Iron Age tech'
+  );
 });
-
