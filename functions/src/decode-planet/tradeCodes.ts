96e379e2fb4ed825ad058e7d08b3757d63c73d3b
Andres Castillo Alarcon <acastillo.trabajo+git@gmail.com>
2026-02-09 13:53:34 +0100

    adding trade codes

--- functions/src/decode-planet/tradeCodes.ts
+++ functions/src/decode-planet/tradeCodes.ts
@@ -5,6 +5,7 @@ interface TradeCode {
 
 const tradeCodesByCondition: Record<string, TradeCode> = {
   As: { abbreviation: 'As', name: 'Asteroid' },
+  Ba: { abbreviation: 'Ba', name: 'Barren' },
   De: { abbreviation: 'De', name: 'Desert' },
   Fl: { abbreviation: 'Fl', name: 'Fluid Hydrospheres' },
   Ga: { abbreviation: 'Ga', name: 'Garden' },
@@ -30,6 +31,7 @@ const tradeCodesByCondition: Record<string, TradeCode> = {
   Mi: { abbreviation: 'Mi', name: 'Mining' },
   Mu: { abbreviation: 'Mu', name: 'Multicultural' },
   Na: { abbreviation: 'Na', name: 'Non-agricultural' },
+  Ni: { abbreviation: 'Ni', name: 'Non-industrial' },
   Pe: { abbreviation: 'Pe', name: 'Penal Settlement' },
   Re: { abbreviation: 'Re', name: 'Reserve' },
   Rs: { abbreviation: 'Rs', name: 'Research Station' },
@@ -39,25 +41,26 @@ const tradeCodesByCondition: Record<string, TradeCode> = {
 };
 
 // Individual code check functions
-function checkAsteroid(sizeCode: string): boolean {
-  return sizeCode === '0';
+function checkAsteroid(sizeCode: string, atmosphereCode: string, hydrographicsCode: string): boolean {
+  return sizeCode === '0' && atmosphereCode === '0' && hydrographicsCode === '0';
 }
 
 function checkDesert(atmosphereCode: string, hydrographicsCode: string): boolean {
-  const thinAtmosphere = ['0', '1', '2', '3'];
-  const lowHydro = ['0', '1'];
-  return thinAtmosphere.includes(atmosphereCode) && lowHydro.includes(hydrographicsCode);
+  const aridAtmosphere = ['2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+  return aridAtmosphere.includes(atmosphereCode) && hydrographicsCode === '0';
 }
 
-function checkFluidHydrospheres(atmosphereCode: string): boolean {
-  const exoticAtmosphere = ['A', 'B', 'C'];
-  return exoticAtmosphere.includes(atmosphereCode);
+function checkFluidHydrospheres(atmosphereCode: string, hydrographicsCode: string): boolean {
+  const veryHighAtmosphere = ['A', 'B', 'C', 'D', 'E', 'F'];
+  const nonZeroHydro = ['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+  return veryHighAtmosphere.includes(atmosphereCode) && nonZeroHydro.includes(hydrographicsCode);
 }
 
-function checkGarden(atmosphereCode: string, hydrographicsCode: string): boolean {
+function checkGarden(sizeCode: string, atmosphereCode: string, hydrographicsCode: string): boolean {
+  const validSize = ['6', '7', '8'];
   const gardenAtmosphere = ['5', '6', '8'];
-  const highHydro = ['4', '5', '6', '7', '8', '9'];
-  return gardenAtmosphere.includes(atmosphereCode) && highHydro.includes(hydrographicsCode);
+  const validHydro = ['5', '6', '7'];
+  return validSize.includes(sizeCode) && gardenAtmosphere.includes(atmosphereCode) && validHydro.includes(hydrographicsCode);
 }
 
 function checkHellworld(atmosphereCode: string): boolean {
@@ -65,9 +68,10 @@ function checkHellworld(atmosphereCode: string): boolean {
   return hellAtmosphere.includes(atmosphereCode);
 }
 
-function checkIceCapped(hydrographicsCode: string): boolean {
-  const iceHydro = ['1', '2', '3'];
-  return iceHydro.includes(hydrographicsCode);
+function checkIceCapped(atmosphereCode: string, hydrographicsCode: string): boolean {
+  const coldAtmosphere = ['0', '1'];
+  const nonZeroHydro = ['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+  return coldAtmosphere.includes(atmosphereCode) && nonZeroHydro.includes(hydrographicsCode);
 }
 
 function checkOceanWorld(hydrographicsCode: string, populationCode: string): boolean {
@@ -81,34 +85,41 @@ function checkVacuum(atmosphereCode: string): boolean {
 }
 
 function checkWaterWorld(hydrographicsCode: string, populationCode: string): boolean {
-  const veryHighHydro = ['A', 'B', 'C'];
-  const lowPopulation = ['0', '1', '2', '3', '4', '5', '6'];
-  return veryHighHydro.includes(hydrographicsCode) && !lowPopulation.includes(populationCode);
+  const veryHighHydro = ['A', 'B', 'C', 'D', 'E', 'F'];
+  return veryHighHydro.includes(hydrographicsCode);
 }
 
-function checkAgricultural(atmosphereCode: string, hydrographicsCode: string, populationCode: string): boolean {
-  const respirableAtmosphere = ['4', '5', '6', '8', '9'];
-  const highHydro = ['4', '5', '6', '7', '8', '9'];
-  const modPopulation = ['5', '6', '7', '8', '9'];
-  return respirableAtmosphere.includes(atmosphereCode) && highHydro.includes(hydrographicsCode) && modPopulation.includes(populationCode);
+function checkNonAgricultural(atmosphereCode: string, hydrographicsCode: string, populationCode: string): boolean {
+  const lowAtmosphere = ['0', '1', '2', '3'];
+  const lowHydro = ['0', '1', '2', '3'];
+  const highPopulation = ['6', '7', '8', '9', 'A', 'B', 'C'];
+  return lowAtmosphere.includes(atmosphereCode) && lowHydro.includes(hydrographicsCode) && highPopulation.includes(populationCode);
 }
 
-function checkIndustrial(populationCode: string, techLevelCode: string): boolean {
+function checkAgricultural(sizeCode: string, atmosphereCode: string, hydrographicsCode: string): boolean {
+  const validSize = ['4', '5', '6', '7', '8', '9'];
+  const validAtmosphere = ['4', '5', '6', '7', '8'];
+  const validHydro = ['5', '6', '7'];
+  return validSize.includes(sizeCode) && validAtmosphere.includes(atmosphereCode) && validHydro.includes(hydrographicsCode);
+}
+
+function checkIndustrial(atmosphereCode: string, populationCode: string): boolean {
+  const validAtmosphere = ['0', '1', '2', '4', '7', '9'];
   const highPopulation = ['9', 'A', 'B', 'C'];
-  const highTech = ['4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-  return highPopulation.includes(populationCode) && highTech.includes(techLevelCode);
+  return validAtmosphere.includes(atmosphereCode) && highPopulation.includes(populationCode);
 }
 
-function checkRich(populationCode: string, governmentCode: string): boolean {
-  const highPopulation = ['9', 'A', 'B', 'C'];
-  const richGovernment = ['4', '6', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-  return highPopulation.includes(populationCode) && richGovernment.includes(governmentCode);
+function checkRich(atmosphereCode: string, hydrographicsCode: string, governmentCode: string): boolean {
+  const allowedAtmosphere = ['6', '8'];
+  const highHydro = ['6', '7', '8'];
+  const governmentRange = ['4', '5', '6', '7', '8', '9'];
+  return allowedAtmosphere.includes(atmosphereCode) && highHydro.includes(hydrographicsCode) && governmentRange.includes(governmentCode);
 }
 
-function checkPoor(populationCode: string, governmentCode: string): boolean {
-  const lowPopulation = ['0', '1', '2', '3', '4', '5'];
-  const poorGovernment = ['2', '3', '4', '7', '9'];
-  return lowPopulation.includes(populationCode) && poorGovernment.includes(governmentCode);
+function checkPoor(atmosphereCode: string, hydrographicsCode: string): boolean {
+  const aridToThinAtmosphere = ['2', '3', '4', '5'];
+  const lowHydro = ['0', '1', '2', '3'];
+  return aridToThinAtmosphere.includes(atmosphereCode) && lowHydro.includes(hydrographicsCode);
 }
 
 function checkPreAgricultural(populationCode: string, techLevelCode: string): boolean {
@@ -129,8 +140,13 @@ function checkHighPopulation(populationCode: string): boolean {
 }
 
 function checkLowPopulation(populationCode: string): boolean {
-  const lowPopulation = ['0', '1'];
+  const lowPopulation = ['0', '1', '2', '3'];
   return lowPopulation.includes(populationCode);
+}
+
+function checkNonIndustrial(populationCode: string): boolean {
+  const midPopulation = ['4', '5', '6'];
+  return midPopulation.includes(populationCode);
 }
 
 function checkHighTech(techLevelCode: string): boolean {
@@ -139,41 +155,60 @@ function checkHighTech(techLevelCode: string): boolean {
 }
 
 function checkLowTech(techLevelCode: string): boolean {
-  const lowTech = ['0', '1', '2', '3'];
+  const lowTech = ['0', '1', '2', '3', '4', '5'];
   return lowTech.includes(techLevelCode);
 }
 
+function checkBarren(populationCode: string, governmentCode: string, lawLevelCode: string): boolean {
+  return populationCode === '0' && governmentCode === '0' && lawLevelCode === '0';
+}
+
+interface TradeCodeRule {
+  code: string;
+  check: (...args: string[]) => boolean;
+  getArgs: (codes: Record<string, string>) => string[];
+}
+
+const tradeCodeRules: TradeCodeRule[] = [
+  { code: 'As', check: checkAsteroid, getArgs: (c) => [c.size, c.atmosphere, c.hydrographics] },
+  { code: 'Ba', check: checkBarren, getArgs: (c) => [c.population, c.government, c.lawLevel] },
+  { code: 'De', check: checkDesert, getArgs: (c) => [c.atmosphere, c.hydrographics] },
+  { code: 'Fl', check: checkFluidHydrospheres, getArgs: (c) => [c.atmosphere, c.hydrographics] },
+  { code: 'Ga', check: checkGarden, getArgs: (c) => [c.size, c.atmosphere, c.hydrographics] },
+  { code: 'He', check: checkHellworld, getArgs: (c) => [c.atmosphere] },
+  { code: 'Ic', check: checkIceCapped, getArgs: (c) => [c.atmosphere, c.hydrographics] },
+  { code: 'Oc', check: checkOceanWorld, getArgs: (c) => [c.hydrographics, c.population] },
+  { code: 'Va', check: checkVacuum, getArgs: (c) => [c.atmosphere] },
+  { code: 'Wa', check: checkWaterWorld, getArgs: (c) => [c.hydrographics] },
+  { code: 'Na', check: checkNonAgricultural, getArgs: (c) => [c.atmosphere, c.hydrographics, c.population] },
+  { code: 'Ni', check: checkNonIndustrial, getArgs: (c) => [c.population] },
+  { code: 'Ag', check: checkAgricultural, getArgs: (c) => [c.size, c.atmosphere, c.hydrographics] },
+  { code: 'In', check: checkIndustrial, getArgs: (c) => [c.atmosphere, c.population] },
+  { code: 'Ri', check: checkRich, getArgs: (c) => [c.atmosphere, c.hydrographics, c.government] },
+  { code: 'Po', check: checkPoor, getArgs: (c) => [c.atmosphere, c.hydrographics] },
+  { code: 'Pr', check: checkPreAgricultural, getArgs: (c) => [c.population, c.techLevel] },
+  { code: 'Mr', check: checkPreIndustrial, getArgs: (c) => [c.population, c.techLevel] },
+  { code: 'Hi', check: checkHighPopulation, getArgs: (c) => [c.population] },
+  { code: 'Lo', check: checkLowPopulation, getArgs: (c) => [c.population] },
+  { code: 'Ht', check: checkHighTech, getArgs: (c) => [c.techLevel] },
+  { code: 'Lt', check: checkLowTech, getArgs: (c) => [c.techLevel] },
+];
+
 function generateTradeCodes(planetCode: string): string[] {
-  const sizeCode = planetCode[1];
-  const atmosphereCode = planetCode[2];
-  const hydrographicsCode = planetCode[3];
-  const populationCode = planetCode[4];
-  const governmentCode = planetCode[5];
-  const techLevelCode = planetCode[7];
+  const planetCodes = {
+    size: planetCode[1],
+    atmosphere: planetCode[2],
+    hydrographics: planetCode[3],
+    population: planetCode[4],
+    government: planetCode[5],
+    lawLevel: planetCode[6],
+    techLevel: planetCode[7],
+  };
 
-  const codes: string[] = [];
+  const codes = tradeCodeRules
+    .filter((rule) => rule.check(...rule.getArgs(planetCodes)))
+    .map((rule) => rule.code);
 
-  if (checkAsteroid(sizeCode)) codes.push('As');
-  if (checkDesert(atmosphereCode, hydrographicsCode)) codes.push('De');
-  if (checkFluidHydrospheres(atmosphereCode)) codes.push('Fl');
-  if (checkGarden(atmosphereCode, hydrographicsCode)) codes.push('Ga');
-  if (checkHellworld(atmosphereCode)) codes.push('He');
-  if (checkIceCapped(hydrographicsCode)) codes.push('Ic');
-  if (checkOceanWorld(hydrographicsCode, populationCode)) codes.push('Oc');
-  if (checkVacuum(atmosphereCode)) codes.push('Va');
-  if (checkWaterWorld(hydrographicsCode, populationCode)) codes.push('Wa');
-  if (checkAgricultural(atmosphereCode, hydrographicsCode, populationCode)) codes.push('Ag');
-  if (checkIndustrial(populationCode, techLevelCode)) codes.push('In');
-  if (checkRich(populationCode, governmentCode)) codes.push('Ri');
-  if (checkPoor(populationCode, governmentCode)) codes.push('Po');
-  if (checkPreAgricultural(populationCode, techLevelCode)) codes.push('Pr');
-  if (checkPreIndustrial(populationCode, techLevelCode)) codes.push('Mr');
-  if (checkHighPopulation(populationCode)) codes.push('Hi');
-  if (checkLowPopulation(populationCode)) codes.push('Lo');
-  if (checkHighTech(techLevelCode)) codes.push('Ht');
-  if (checkLowTech(techLevelCode)) codes.push('Lt');
-
-  // Remove duplicates
   const uniqueCodes = Array.from(new Set(codes));
 
   return uniqueCodes;
